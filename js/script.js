42a117581ba14fa6aef9286b7ae53a738aef4838
Leandro <thesquallrock@gmail.com>
2020-12-11 17:14:50 +0100

    mancava un punto e virgola

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var nome, cognome, colore, numero, password;
 nome = prompt('Qual è il tuo nome?');
 cognome = prompt('E il tuo cognome?');
 colore = prompt('Qual è il tuo colore preferito?');
-numero = 19
+numero = 19;
 password = nome + cognome + colore + numero;
 
 var max = password.charAt(0).toUpperCase();
